302d7ef1332c4b567af5d0879ec858754c2da906
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-05-09 11:10:52 +0300

    project 1: codebase without gradio

--- project1/test_summarizer.py
+++ project1/test_summarizer.py
@@ -15,3 +15,12 @@ def summarize_text(text):
         return response.json().get("response", "No response found")
     else:
         return f"Error: {response.status_code} - {response.text}"
+
+
+if __name__ == "__main__":
+
+    sample_text = """
+        Genshin Impact is an open-world action role-playing game developed and published by miHoYo. It was released for Microsoft Windows, PlayStation 4, Nintendo Switch, iOS, and Android. The game features a fantasy world called Teyvat, which is home to seven distinct nations, each of which is based on a different culture and element. Players take on the role of the Traveler, who is searching for their lost sibling and unraveling the mysteries of Teyvat.
+     """
+    print("Summerize text:")
+    print(summarize_text(sample_text))
